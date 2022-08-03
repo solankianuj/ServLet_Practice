f045b367fdf35779a2cb34a70ca5e492323a10f8
solanki <anujaj2222.gmail.com>
2022-08-03 14:57:55 +0530

    Creating Our First Servlet

--- FirstServlet.java
+++ FirstServlet.java
@@ -14,7 +14,7 @@ public class FirstServlet  extends HttpServlet{
     protected void doGet(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException {
             response.setContentType("text/html");
             PrintWriter out = response.getWriter();
-            out.println("<h3>Hello World Again</h3>");
+            out.println("<h3>Hello World Its Anuj Again</h3>");
             out.close();
         }
     }
